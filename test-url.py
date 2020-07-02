ff31d1f18ec6224ea0dfbeaa36ddc54a520efd3f
Kirill <53617355+smolovk@users.noreply.github.com>
2020-07-02 21:51:57 +0300

    Update test-url.py

--- test-url.py
+++ test-url.py
@@ -4,7 +4,7 @@ urls = ["https://ftl.kherson.ua/"]
 
 for url in urls:
     print(f'Analyzing {url}')
-    for i in range(10):
+    for i in range(100):
         r = requests.get(url)
         if r.status_code != 200:
             print(f'WOW!!!, found code {r.status_code} on {i} iteration of analyzing {url}')
